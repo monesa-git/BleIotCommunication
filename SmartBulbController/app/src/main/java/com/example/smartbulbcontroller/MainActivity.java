6f916958e97f2ccfbed4d960eb8eb3584583eeac
Monesa <mthogulu@uncc.edu>
2020-11-09 23:18:42 -0500

    Smart bulb implemented - Havent added Progress bar yet

--- SmartBulbController/app/src/main/java/com/example/smartbulbcontroller/MainActivity.java
+++ SmartBulbController/app/src/main/java/com/example/smartbulbcontroller/MainActivity.java
@@ -9,7 +9,11 @@ import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
@@ -21,15 +25,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     // Initializes Bluetooth adapter.
     BluetoothManager bluetoothManager;
@@ -38,6 +46,25 @@ public class MainActivity extends AppCompatActivity {
     private boolean mScanning;
     BluetoothGatt bluetoothGatt;
     ScanCallback leScanCallback;
+
+    private Button onButton;
+    private Button OffButton;
+    private Button OnBeepButton;
+    private Button OffBeepButton;
+    private ImageView bulbImage;
+    TextView TemperatureNotify;
+    private BluetoothAdapter.LeScanCallback mleScanCallback;
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+
+//    df5302f9-537b-aed4-0618-1ebe135a1000 - Chandu
+//    df9f381b-e0b6-5564-8528-b4c0ab556265 = chottu
+//    dfc06d52-2190-322b-b6d3-39b595b463 = mine
+
+    public static final String bleApp = "df9f381b-e0b6-5564-8528-b4c0ab556265";
+    public static final String bulb = "FB959362-F26E-43A9-927C-7E17D8FB2D8D";
+    public static final String temp = "0CED9345-B31F-457D-A6A2-B3DB9B03E39A";
+    public static final String beep = "EC958823-F26E-43A9-927C-7E17D8F32A90";
 
 
     //static variables
@@ -52,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler = new Handler();
     EditText et_deviceList;
     boolean is_loaction_Enable = false;
+    private int gattStatus;
+
+    private int connectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
 
     ///// temporary
     int limit = 0;  // for no. of calls for flow
@@ -61,25 +93,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //if device does not support ble, app is of no use ==> it will close itself
         CheckIfDeviceSupportBLE();
+        onButton = findViewById(R.id.OnButton);
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothGattCharacteristic characteristic = (BluetoothGattCharacteristic) v
+                        .getTag();
+                characteristic.setValue(new byte[] { (byte) 0x03 });
+                if (bluetoothGatt.writeCharacteristic(characteristic)) {
+                    Log.d("demo","Bulb switched on");
+                }
+            }
+        });
 
+        OffButton = findViewById(R.id.OffButton);
+        OffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothGattCharacteristic characteristic = (BluetoothGattCharacteristic) v
+                        .getTag();
+                characteristic.setValue(new byte[] { (byte) 0x00 });
+                if (bluetoothGatt.writeCharacteristic(characteristic)) {
+                    Log.d("demo","Bulb switched off");
+                }
+            }
+        });
 
-        /////////////////////////////////   Method 1
-        // getting UI components
-        et_deviceList = findViewById(R.id.ETML_deviceList);
+        OnBeepButton =  findViewById(R.id.onBeepButton);
+        OnBeepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothGattCharacteristic characteristic = (BluetoothGattCharacteristic) v
+                        .getTag();
+                characteristic.setValue(new byte[] { (byte) 0x03 });
+                if (bluetoothGatt.writeCharacteristic(characteristic)) {
+                    Log.d("demo","Beep sound is on");
+                }
+            }
+        });
 
-        // flow
-        // CheckIfPermissionGranted();  then
-        // CheckIfBTisEnabled() then
-        // CheckIfLocationIsOn() then start bluetooth process
+        OffBeepButton = findViewById(R.id.offBeepButton);
+        OffBeepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothGattCharacteristic characteristic = (BluetoothGattCharacteristic) v
+                        .getTag();
+                characteristic.setValue(new byte[] { (byte) 0x00 });
+                if (bluetoothGatt.writeCharacteristic(characteristic)) {
+                    Log.d("demo","Beep sound is off");
+                }
+            }
+        });
+
+        TemperatureNotify= findViewById(R.id.temperatureText);
+
         FlowToScanForBLEDevices();
-        //////////////////////////////////  Method 1 End
-
-
-//        ConnectToBTDevice();
-
     }
     void FlowToScanForBLEDevices(){
         limit++;
@@ -92,34 +162,117 @@ public class MainActivity extends AppCompatActivity {
             bluetoothAdapter = bluetoothManager.getAdapter();
 
            if (CheckIfBTisEnabled() && CheckIfLocationIsOn()){
-               ////////// method 1
-//               bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
-//               ScanForBLEDevices();
-               ///////// method 1 end
-
-               ///////// method 2
-               BluetoothAdapter.LeScanCallback mleScanCallback =  new BluetoothAdapter.LeScanCallback() {
+               mleScanCallback =  new BluetoothAdapter.LeScanCallback() {
                    @Override
                    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                BTs.add(device);
+                                Log.d("demo", "got the device finally!");
                                 Log.d(TAG, "onScanResult: BT Device found=>"+device.getName()+"  Addresss=>"+device.getAddress());
-                                String rs  = "";
-                                for (BluetoothDevice b : BTs){
-                                    rs+=b.getName()+"\n";
-                                }
-                                et_deviceList.setText(rs);
+                                bluetoothAdapter.stopLeScan(mleScanCallback);
+                                bluetoothGatt = device.connectGatt(MainActivity.this, false, gattCallback);
                             }
                         });
                    }
                };
-               Log.d(TAG, "FlowToScanForBLEDevices: le scan started=>"+bluetoothAdapter.startLeScan(new UUID[]{UUID.fromString("dfad74bb-6584-b699-4be5-6d8b8076ca97")},mleScanCallback));
-               ////////
+
+               Log.d(TAG, "FlowToScanForBLEDevices: le scan started=>"+
+                       bluetoothAdapter.startLeScan(new UUID[]{UUID.fromString(bleApp)},
+                               mleScanCallback));
            }
         }
     }
+
+    private final BluetoothGattCallback gattCallback =
+            new BluetoothGattCallback() {
+                @Override
+                public void onConnectionStateChange(BluetoothGatt gatt, int status,
+                                                    int newState) {
+                    Log.d("demo",gatt.getServices().toString());
+                    Log.d("demo","OnConnectionStateChange");
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        Log.i(TAG, "Connected to GATT server.");
+                        Log.i(TAG, "Attempting to start service discovery:" +
+                                bluetoothGatt.discoverServices());
+                        gattStatus = newState;
+//                        bluetoothGatt.discoverServices();
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        Log.i(TAG, "Disconnected from GATT server.");
+                    }
+                }
+
+                @Override
+                // New services discovered
+                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                    Log.d("demo","onServicesDiscovered");
+                    for (BluetoothGattService service : gatt.getServices()) {
+                        if ((service == null) || (service.getUuid() == null)) {
+                            continue;
+                        }
+
+                        if (bleApp.equalsIgnoreCase(service
+                                .getUuid().toString())) {
+                            onButton.setTag(service.getCharacteristic(UUID
+                                            .fromString(bulb)));
+                            OffButton.setTag(service.getCharacteristic(UUID
+                                            .fromString(bulb)));
+                            OnBeepButton.setTag(service.getCharacteristic(UUID
+                                            .fromString(beep)));
+                            OffBeepButton.setTag(service.getCharacteristic(UUID
+                                            .fromString(beep)));
+                            TemperatureNotify.setTag(service.getCharacteristic(UUID
+                                    .fromString(temp)));
+
+                            BluetoothGattCharacteristic characteristic =service.getCharacteristic(UUID
+                                    .fromString(temp));
+
+                            gatt.setCharacteristicNotification(characteristic,true);
+                            gatt.readCharacteristic(characteristic);
+                        }
+                    }
+                }
+
+                @Override
+                // Result of a characteristic read operation
+                public void onCharacteristicRead(BluetoothGatt gatt,
+                                                 BluetoothGattCharacteristic characteristic,
+                                                 int status) {
+                    Log.d("demo", "onCharacteristicRead");
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        if (temp.equalsIgnoreCase(characteristic.getUuid().toString())) {
+                            final String tempValue = characteristic.getStringValue(0);
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Log.d("demo", "temperature value" + tempValue);
+                                    TemperatureNotify.setText(tempValue + " F");
+                                }
+                            });
+                        }
+                    }
+                }
+
+                @Override
+                // Characteristic notification
+                public void onCharacteristicChanged(BluetoothGatt gatt,
+                                                    BluetoothGattCharacteristic characteristic) {
+                    if (temp.equalsIgnoreCase(characteristic.getUuid().toString())) {
+                        final String tempValue = characteristic.getStringValue(0);
+                        Log.d("demo","temperature value on notify"+tempValue);
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+//                                Log.d("demo","temperature value on notify"+tempValue);
+                                TemperatureNotify.setText(tempValue + " F");
+                            };
+                        });
+                    }
+                }
+
+                @Override
+                public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                    super.onCharacteristicWrite(gatt, characteristic, status);
+                }
+            };
 
     private boolean CheckIfLocationIsOn() {
         Log.d(TAG, "CheckIfLocationIsOn: called");
@@ -188,28 +341,6 @@ public class MainActivity extends AppCompatActivity {
 
         // trying to get only one result
         bluetoothLeScanner.startScan(leScanCallback);
-
-//        if (!mScanning) {
-//            // Stops scanning after a pre-defined scan period.
-//            handler.postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    mScanning = false;
-//                    Toast.makeText(MainActivity.this, "10 second over", Toast.LENGTH_SHORT).show();
-//                    bluetoothLeScanner.stopScan(leScanCallback);
-//                }
-//            }, SCAN_PERIOD);
-//
-//            Log.d(TAG, "ScanForBLEDevices: started scanning for devices");
-//            mScanning = true;
-//            bluetoothLeScanner.startScan(leScanCallback);
-//            //for scanning specific UUIDs
-//            //bluetoothLeScanner.startScan([''],leScanCallback);
-//        } else {
-//            mScanning = false;
-//            bluetoothLeScanner.stopScan(leScanCallback);
-//        }
-
 
     }
 
